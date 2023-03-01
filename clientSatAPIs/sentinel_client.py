88010f753e5761aff5e006acd3ec374e1263dccd
sparkvilla <diego.villamaina@gmail.com>
2023-03-01 14:01:00 +0100

    changed tile names

--- clientSatAPIs/sentinel_client.py
+++ clientSatAPIs/sentinel_client.py
@@ -14,26 +14,30 @@ from sentinelsat import SentinelAPI
 
 # connect to the API
 # A .netrc file with username and password must be present in the home folder
-api = SentinelAPI(None, None, 'https://scihub.copernicus.eu/dhus')
+api = SentinelAPI(None, None, "https://scihub.copernicus.eu/dhus")
 
-OUTFILE = '/home/diego/work/dev/sent_client.log'
-TILES = ['*T20MNA*', '*T20MPA*', '*T20MQA*',
-         '*T20MNV*', '*T20MPV*', '*T20MQV*',
-         '*T20MNB*', '*T20MPB*', '*T20MQB*']
+OUTFILE = "/home/diego/work/dev/sent_client.log"
+TILES = ["*T20QME*", "*T20QNE*"]
+import pdb
+
 
 for tl in TILES:
-
-    products = api.query(platformname='Sentinel-2', filename=tl, cloudcoverpercentage=(0, 1))
-
-    with open(OUTFILE, 'a') as f:
-        f.write(f'Tile: {tl}\n')
+    products = api.query(
+        platformname="Sentinel-2",
+        # processingLevel="Level-2A",
+        date=("20150101", "20170101"),
+        filename=tl,
+        cloudcoverpercentage=(0, 5),
+    )
+    with open(OUTFILE, "a") as f:
+        f.write(f"Tile: {tl}\n")
 
         for k in products.keys():
-            begin_acquisition_date = products.get(k)['beginposition'].date()
+            begin_acquisition_date = products.get(k)["beginposition"].date()
             date = begin_acquisition_date.strftime("%m/%d/%Y")
-            size = products.get(k)['size']
-            uuid = products.get(k)['uuid']
-            f.write(f'{date}, {size}, {uuid}\n')
+            size = products.get(k)["size"]
+            uuid = products.get(k)["uuid"]
+            f.write(f"{date}, {size}, {uuid}\n")
 
-        f.write('\n')
-        f.write('\n')
+        f.write("\n")
+        f.write("\n")
